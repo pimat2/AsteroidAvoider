12a1c06ae7d3879d30b94f5bfc05e5a308e12b25
pimat2 <91675243+pimat2@users.noreply.github.com>
2023-01-31 17:17:02 +0200

    HighScore System
    The highest score the player has achieved will always be displayed in the mainMenu Scen

--- AsteroidAvoider/Assets/Scripts/MainMenu.cs
+++ AsteroidAvoider/Assets/Scripts/MainMenu.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI highScoreText;
+    void Start() {
+        OnApplicationFocus(true); 
+    } 
+    private void OnApplicationFocus(bool focusStatus) {
+        int highScore = PlayerPrefs.GetInt(ScoreHandler.HighScoreKey, 0);
+        highScoreText.text = $"Your highest score is: {highScore}";
+    }
     public void PlayGame(){
         SceneManager.LoadScene("MainScene");
     }
--- AsteroidAvoider/Assets/Scripts/ScoreHandler.cs
+++ AsteroidAvoider/Assets/Scripts/ScoreHandler.cs
@@ -7,6 +7,7 @@ public class ScoreHandler : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI scoreText;
     [SerializeField] float scoreMultiplier;
+    public const string HighScoreKey = "HighScore";
     bool isCounting = true;
     float score;
     void Update()
@@ -24,5 +25,11 @@ public class ScoreHandler : MonoBehaviour
         isCounting = false;
         scoreText.text = string.Empty;
         return Mathf.FloorToInt(score);
+    }
+    void OnDestroy() {
+        int currentHighScore = PlayerPrefs.GetInt(HighScoreKey, 0);
+        if(score > currentHighScore){
+            PlayerPrefs.SetInt(HighScoreKey, Mathf.FloorToInt(score));
+        }    
     }
 }
