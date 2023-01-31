ac0e74e7faa403f4187f92d78229d344d61fd15e
pimat2 <91675243+pimat2@users.noreply.github.com>
2023-01-31 03:58:16 +0200

    Backgorund Scrolling Effect

---
+++ AsteroidAvoider/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+   private void OnTriggerEnter(Collider other) {
+        if(other.GetComponent<PlayerHealth>()){
+            other.GetComponent<PlayerHealth>().Crash();
+        }
+        else{
+            return;
+        }
+   }
+}
